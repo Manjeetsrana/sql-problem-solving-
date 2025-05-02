debecbca68c410e36a4c6078209e9aed78a3ea30
Manjeetsrana <133259013+Manjeetsrana@users.noreply.github.com>
2025-05-02 22:52:17 +0530

    Update employee_info.sql

--- sql-scripts/employee_info.sql
+++ sql-scripts/employee_info.sql
@@ -3,5 +3,13 @@
 
 --  Find the percentage of people who work in each department. 
 
-select department, count(employee_id) as dep_wise, count(employee_id)/(select count(*) from employees_info) *100 as percent from employees_info 
-group by department	
+SELECT 
+    department,
+    COUNT(employee_id) AS dep_wise,
+    COUNT(employee_id) / (SELECT 
+            COUNT(*)
+        FROM
+            employees_info) * 100 AS percent
+FROM
+    employees_info
+GROUP BY department	
